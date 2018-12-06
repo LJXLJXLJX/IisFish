86b0691f7053cd7d5cff007700e8ea6cc146a7ec
1042785680 <1042785680@qq.com>
2018-12-06 19:02:04 +0800

    118

--- Assets/Scripts/CatRunToTarget.cs
+++ Assets/Scripts/CatRunToTarget.cs
@@ -50,7 +50,7 @@ public class CatRunToTarget : MonoBehaviour
     {
         if (collision.collider.tag == "Player")
         {
-            FishDie fd = collision.collider.gameObject.GetComponent<FishDie>();
+            Fish fd = collision.collider.gameObject.GetComponent<Fish>();
             fd.fishDie();
             rb.velocity = Vector2.zero;
             Invoke("resetCat", 1.5f);
